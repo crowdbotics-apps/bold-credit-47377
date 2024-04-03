be5fed7200cca67216c40f915163a643cc047cb5
Crowdbotics <team@crowdbotics.com>
2024-04-03 14:59:15 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "bold-credit-47377",
+  url: "https://bold-credit-47377.botics.co",
+  api: "https://bold-credit-47377.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
